ac7fbdec3840f4c4153658920fb861ff925185be
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-09-01 22:39:37 -0400

    [ADD]: signs added

---
+++ util/signs.js
@@ -0,0 +1,14 @@
+module.exports = [
+  "Virgo",
+  "Libra",
+  "Scorpio",
+  "Sagittarius",
+  "Capricorn",
+  "Aquarius",
+  "Pisces",
+  "Aries",
+  "Taurus",
+  "Gemini",
+  "Cancer",
+  "Leo"
+];
